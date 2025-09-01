bd68ce59bb54eee66a8b5c20c6f16f41671ac9cd
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-01 18:41:06 +0100

    Simple operations

---
+++ homework01.py
@@ -0,0 +1,15 @@
+#autor:karla patino
+#date:1st/sep/2025
+#description: program to resquest user name as input and return same withg a greeting
+user_first_name= input('first name : ')
+user_surname=input('surname : ')
+print('hello',user_first_name,user_surname)
+num1 = input('enter first number : ')
+num2 = input('enter second number : ')
+num3 = input('enter third number : ')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+total1=(num1+num2)
+result=(total1*num3)
+print('the answer is',result)
