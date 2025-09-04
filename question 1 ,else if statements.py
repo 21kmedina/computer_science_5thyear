8f911c2b0f408ed42566f4da4ca8ac99b29730d5
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-04 20:32:54 +0100

    homework

--- question 1 ,else if statements.py
+++ question 1 ,else if statements.py
@@ -7,4 +7,9 @@ if (varb_a < varb_b):
     print(varb_a,varb_b)
 else:
     print(varb_b,varb_a)
-    
+varb_a=input('choose a value for a :')
+varb_b=input('choose a value for b :')
+if (varb_a > varb_b):
+    print(varb_b,varb_a)
+else:
+    print(varb_a,varb_b) 
---
+++ question2,else if staements.py
@@ -0,0 +1,10 @@
+#author:karla patino
+# date: 4th september, 2025,thursday
+#des-cription: question 2 , else if statements
+var1=int(input('enter a number UNDER 20 :'))
+und20= int(20)
+if (var1 < und20 ):
+    print('thank you (:')
+else:
+    print('INCORRECT VALUE!!')
+    
