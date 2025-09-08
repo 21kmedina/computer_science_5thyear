61b6849e0e27a7ea8e9180c9bbf5854d47408ad4
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-08 12:50:41 +0100

    Add files via upload

---
+++ loops qstn 2.py
@@ -0,0 +1,10 @@
+#author:karla patino
+#date:8th/sep/2025
+#for loops, question 2
+
+name=input('enter your name :')
+
+repeat=int(input('enter a number:'))
+
+for i in range(repeat):
+    print(name)
