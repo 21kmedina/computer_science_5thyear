0be4e98933384f82568237ea97e30028770c0ab8
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-09 14:11:04 +0100

    Add files via upload

---
+++ intro to division.py
@@ -0,0 +1,8 @@
+#to test if a number is even, take the
+#number modulo (%) 2
+#modulo gives the reminder after division
+#if you divide by 2,the remainder is 0
+#for an even number and 1 for an odd number
+
+print(4%2)
+print(5%2)
