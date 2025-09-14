9ef274e7190d5df4f05971cae332e3d57971c5d0
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-14 15:29:52 +0100

    Add files via upload

---
+++ assignment 1/question 4.py
@@ -0,0 +1,11 @@
+print("enter three numbers")
+firstnum=input("enter first number :")
+secondnum=input("enter second number:")
+thirdnum=input("enter third number:")
+num1=int(firstnum)
+num2=int(secondnum)
+num3=int(thirdnum)
+add1=num1+num2
+add2=int(add1)
+mult=add2*num3
+print("the answer is:",mult)
