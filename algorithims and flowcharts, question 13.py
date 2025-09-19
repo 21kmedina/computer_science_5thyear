b5497cda8fa6f14a7abbe59772c45cb90fb3a7c0
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-19 09:44:59 +0100

    Add files via upload

---
+++ algorithims and flowcharts, question 13.py
@@ -0,0 +1,12 @@
+'''
+author: Karla Patino Medina
+Date:19th of september 2025
+description:flowcharts excercise,checks if a number is odd or even
+
+'''
+num=input('Enter a number:')
+num=int(num)
+if(num % 2==0):
+    print('this is an even number!')
+else:
+    print('this is an odd number')
