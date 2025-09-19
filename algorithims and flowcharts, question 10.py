4a9ed2cad0766bced8373a4f9064fa24e9201794
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-19 09:33:14 +0100

    Add files via upload

---
+++ algorithims and flowcharts, question 10.py
@@ -0,0 +1,19 @@
+'''
+author: Karla Patino Medina
+Date:19th of september 2025
+description:flowcharts excercise, question 9
+
+'''
+
+p=input('enter how much money you have:')
+p=float(p)
+
+r=input('enter the amount of annual interest rate you have:')
+r=float(r)
+
+t=input('how many years:')
+t=float(t)
+
+simpleinterest=(p*r*t/100)
+
+print(simpleinterest)
