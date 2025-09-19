507eeb52020be341994695e89ed73ded9fb96dcb
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-19 09:21:39 +0100

    Add files via upload

---
+++ algorithims and flowcharts, question 9.py
@@ -0,0 +1,15 @@
+'''
+author: Karla Patino Medina
+Date:19th of september 2025
+description:flowcharts excercise
+
+'''
+
+radius=input('enter the radius of the circle :')
+radius=float(radius)
+
+area=float(radius*3.1415926535* 3.1415926535)
+print('this is the area of the circle :',area)
+
+circumference=(2* 3.1415926535*radius)
+print('the circumference of this circle is:',circumference,)
