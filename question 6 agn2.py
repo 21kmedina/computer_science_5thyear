3129376b664d53ee8af5ab639ceb0065c6ebf0e6
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-25 19:26:25 +0100

    Add files via upload

---
+++ question 6 agn2.py
@@ -0,0 +1,10 @@
+num=input("enter your age :")
+num==int
+if intnum:=18:
+    print("you can vote")
+elif num:=17:
+    print("you can learn to dirve")
+elif num:=16:
+    print("you can buy a lottery ticket")
+else:
+    print("you can go trick or treating")
