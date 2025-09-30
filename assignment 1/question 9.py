5fef89a54ff98a50db5cf02d2cc09820d68ff0e5
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-30 13:50:37 +0100

    Update question 9.py

--- assignment 1/question 9.py
+++ assignment 1/question 9.py
@@ -1,3 +1,6 @@
+#autor:karla patino
+#date:1st/sep/2025
+#description: weight to pounds
 weightkg=input('enter a weight in kilograms:')
 pounds=int(weightkg)*2.204
 print("there are",pounds,"pounds in",weightkg,"kilograms")
