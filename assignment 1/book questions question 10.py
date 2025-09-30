a78fe6a24deb4be1aaa2423fcfa8555cc5987562
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-30 14:35:37 +0100

    Add files via upload

---
+++ assignment 1/book questions question 10.py
@@ -0,0 +1,11 @@
+#autor:karla patino
+#date:/1stsep/2025
+#description: question 10,
+principal=input('values for principal:')
+interest_rate=input('values for interest rate:')
+time=input('values for time in years:')
+principal=int(principal)
+interest_rate=int(interest_rate)
+time=int(time)
+simple_interest=int(principal*interest_rate*time)
+print('simple interest is',simple_interest)
