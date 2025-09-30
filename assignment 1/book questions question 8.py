794b69e50225d2e9c73fef0df29047acfecc7e85
Karla <148641305+21kmedina@users.noreply.github.com>
2025-09-30 14:20:43 +0100

    Add files via upload

---
+++ assignment 1/book questions question 8.py
@@ -0,0 +1,9 @@
+#autor:karla patino
+#date:/1stsep/2025
+#description: question 8,height to feet
+heightcm=input('whats your height in cm?:')
+heightcm=int(heightcm)
+inches=float(heightcm/2.54)
+print('your height to inches is ',inches)
+feet=int(inches/12)
+print('your height to feet is ',feet)
