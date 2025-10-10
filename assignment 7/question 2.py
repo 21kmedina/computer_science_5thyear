ec1a5aa030b8d9392b0115fa78c1f75a1f00a7b2
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-10 09:45:34 +0100

    Add files via upload

---
+++ assignment 7/question 2.py
@@ -0,0 +1,25 @@
+'''
+Karla Patino Medina
+10th October 2025
+assignment 7, question 2
+program print positive and negative numbers from user till they say 0
+
+
+'''
+num=0
+count=0
+
+while num!='':
+    num=input('Enter a number:')
+    if num.isdigit():
+        num=int(num)
+        list[num]
+        count+= count +num
+        if (num!=0):
+            continue
+        elif(num==0):
+            break
+        
+    
+        
+    
