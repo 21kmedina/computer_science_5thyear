d4707d315e8fa0e5eb71cfbbc56c705a81af465c
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-17 08:59:40 +0100

    Add files via upload

---
+++ assignmnet 9/question 2.py
@@ -0,0 +1,9 @@
+'''authour karla
+assignmnet 9 question 2
+printing lenght of 2 names'''
+
+first_name=input('enter your first name :')
+second_name=input('enter your second name :')
+print(first_name, second_name)
+print(len(first_name+second_name))
+
