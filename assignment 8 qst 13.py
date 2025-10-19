4cb2149d3f5ed0fb986867cdcfb78a7dc11d29b9
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-19 18:46:30 +0100

    Add files via upload

---
+++ assignment 8 qst 13.py
@@ -0,0 +1,18 @@
+'''
+author: Karla Patino
+date: 16th october 2025
+assigment 8, question 13
+see what it ends with
+'''
+a=input('enter any number: ')
+if(a[-1]=='4'):
+    print('this ends with 4')
+elif(a[-1]=='8'):
+    print('ends with 8')
+else:
+    print('ends with neither')
+
+
+
+
+    
