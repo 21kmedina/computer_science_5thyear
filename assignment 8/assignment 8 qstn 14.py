e78969cfe913223ef4fe77503fc83021257c5ac9
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-19 19:28:12 +0100

    Add files via upload

---
+++ assignment 8/assignment 8 qstn 14.py
@@ -0,0 +1,12 @@
+'''
+author: Karla Patino
+date: 16th october 2025
+assigment 8, question 14
+'''
+num=0
+while num !='':
+    num=input('enter a number:')
+    if num.isdigit():
+        num=int(num)
+        for i in range(20):
+            print(i)
