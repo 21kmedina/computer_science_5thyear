91c45ba572f9e8b9ed6c415d13026c328b57d203
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-21 14:31:24 +0100

    Add files via upload

---
+++ assignment 10 question 1.py
@@ -0,0 +1,53 @@
+'''
+athour:karla Patino
+21 october of 2025
+'''
+#string1=input('enter a word :')
+#string2=string1.lower()
+#print(string2)
+
+#test1=input('enter  a word :')
+#test1=('orange')
+#test2=test1.count('o')
+#print(test2)
+
+
+#string_4= input('enter a word :')
+#string_4=string_4.find('a')
+#print(string_4)
+
+
+#string_5= input('enter a word :')
+#string_5=string_5.replace('a','o')
+#print(string_5)
+
+#string_6= input('enter a word :')
+#string_6=string_6.islower()
+#print(string_6)
+
+#string_7= input('enter a word :')
+#string_7=string_7.isupper()
+#print(string_7)
+
+
+#string_8= input('enter a word :')
+#string_8=string_8.isalnum()
+#print(string_8)
+
+#string_9= input('enter a word :')
+#string_9=string_9.isdigit()
+#print(string_9)
+
+
+
+#string_10= input('enter a word :')
+#string_10=string_10.index('a')
+#print(string_10)
+
+string_11= input('enter a word :')
+string_11=string_11.strip('x')
+print(string_11)
+
+
+
+
