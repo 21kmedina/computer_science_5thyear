bc749b56705908eb1f684bc93786b31c7572140a
Karla <148641305+21kmedina@users.noreply.github.com>
2025-10-23 09:15:42 +0100

    Add files via upload

---
+++ assignment 10/question 2(ii).py
@@ -0,0 +1,13 @@
+'''
+author Karla Patino Medina
+23rd october 2025
+assignment 10, question 2(ii)
+'''
+lenght = 0
+
+while lenght!='':
+    word=input('Enter a word :')
+    if word.isalpha():
+        lenght=len(word)
+    print(lenght)
+        
