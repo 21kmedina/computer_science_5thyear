06babff4316c3a94fc4375ceeaaf7cf47d67860e
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-04 14:02:44 +0000

    Update question 4.py

--- assignment 11/question 4.py
+++ assignment 11/question 4.py
@@ -1,19 +1,20 @@
 '''
-author: Karla Patino medina
-date: 3rd november 2025
-assignment 11 queston 4
+author: Karla Patino Medina
+date: 4th November 2025
+assignment 11 question 4
 '''
 number=80
 tries=7
-while tries!='':
+while tries!=0:
     guess=input('Enter your guess:')
     guess=int(guess)
     if guess==number:      
         print('CORRECT')
+        break
     elif guess<number:
         print('Too low')
-        tries=-1
+        tries-=1
     elif guess>number:
         print('Too high')
-        tries=-1
+        tries-=1
     
