ec45939967f650e1a0812b0e85d4d0aff7eb64b2
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-06 09:46:36 +0000

    Add files via upload

---
+++ assignment 12/question 12.py
@@ -0,0 +1,17 @@
+'''
+Name: Karla Patino Medina
+Date:5th of November 2025
+Assignmnet 12, question 12, questions from the book
+'''
+
+
+formula=input('Enter a sentence with brackets:')
+brackets1=formula.count('(')
+brackets2=formula.count(')')
+print(brackets1+brackets2)
+amount=int(brackets1+brackets2)
+if amount%2==0:
+    print('Theres an even amount of brackerts')
+else:
+    print('not equal too')
+
