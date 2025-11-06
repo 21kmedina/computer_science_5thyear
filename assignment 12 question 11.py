f7c09c89a5c2a24ac78fa3508376a875d3b49382
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-06 09:27:03 +0000

    Add files via upload

---
+++ assignment 12 question 11.py
@@ -0,0 +1,11 @@
+'''
+Name: Karla Patino Medina
+Date:5th of November 2025
+Assignmnet 12, question 11, questions from the book
+'''
+
+string=input('Enter a string:')
+word=string.count(' ')
+print(f'there are {word+1} words in this string')
+    
+
