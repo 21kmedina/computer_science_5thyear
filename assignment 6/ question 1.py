882137a8498ab80827c5116a08d1b26fbaa6d077
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-07 14:51:15 +0000

    Refactor input prompts and average calculation

--- assignment 6/ question 1.py
+++ assignment 6/ question 1.py
@@ -3,10 +3,10 @@
 #description: question 1, while loops excersices
 num=0
 
-while num!='':
-    num1=int(input('Enter a WHOLE number :'))
-    num2=int(input('Enter another WHOLE number :'))
-    num3=int(input('Enter the last WHOLE number:'))
-    add=int(num1+num2+num3)
-    print('the average is',add/3,)
-
+while num!=(' '):
+    num1=int(input('Enter first number:'))
+    num2=int(input('Enter a second number:'))
+    num3=int(input('Enter a third number:'))
+    total=int(num1+num2+num3)
+    print('the average is',total/3)
+    num=(' '):
