ba44a5a84f4052936ec3f478aad14c5708809dba
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-07 09:18:47 +0000

    Add files via upload

---
+++ assignment 12/qyestion 13.py
@@ -0,0 +1,19 @@
+'''
+Name: Karla Patino Medina
+Date:5th of November 2025
+Assignmnet 12, question 13, questions from the book
+'''
+
+word=input('Enter a word:')
+a=word.count('a')
+e=word.count('e')
+i=word.count('i')
+o=word.count('o')
+u=word.count('u')
+A=word.count('A')
+E=word.count('E')
+I=word.count('I')
+O=word.count('O')
+U=word.count('U')
+print(f'there are {a+A+e+E+i+I+o+O+u+U} vowels in this word')
+
