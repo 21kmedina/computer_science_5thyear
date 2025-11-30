866aba5e4de65756be378daf135f427f70f90fe0
Karla <148641305+21kmedina@users.noreply.github.com>
2025-11-30 18:23:13 +0000

    Update hex to binary.py

--- hex to binary.py
+++ hex to binary.py
@@ -9,10 +9,57 @@ step3)take leading zeros away for only the first value(if loop), find binary num
 step4)count amount of digits all together and see which row has the same amount of x's for the amount of digits
 step 5)write the number in row and fill in the x's space w binary digits
 '''
+userInput=input('enter a unicode code to convert to binary : ')
+userInputHexPart=userInput[2::]
 
+decimalValue=0
+finalBinaryValue=''
+for characterInTurn in userInputHexPart:
+    st= ''
+    if characterInTurn=='A':
+        decimalValue=10
+    elif characterInTurn=='B':
+        decimalValue=11
+    elif characterInTurn=='C':
+        decimalValue=12
+    elif characterInTurn=='D':
+        decimalValue=13
+    elif characterInTurn=='E':
+        decimalValue=14
+    elif characterInTurn=='F':
+        decimalValue=15
+    else:
+        decimalValue=int(characterInTurn)
+       
+    ' Convert binary '
+    binaryNum=''
 
-UserInput=input('enter a unicode code to convert to binary :')
-UniCode=int(UserInput)
-firstValue=Unicode[0]
-firstValue  # convert hex to decimal 
-x=x(len[2:])# do not count u+, start hexo to decimal in second postion 
+    while decimalValue > 0:
+        if decimalValue%2==0:
+            binaryNum='0' + binaryNum
+        else:
+            binaryNum='1' + binaryNum
+        decimalValue=decimalValue//2
+
+    if len(binaryNum) == 0:
+        binaryNum='0000'
+    elif len(binaryNum) == 1:
+        binaryNum='000' + binaryNum
+    elif len(binaryNum) == 2:
+        binaryNum='00' + binaryNum
+    elif len(binaryNum) == 3:
+        binaryNum='0' + binaryNum
+
+    print ('binaryNum: ', binaryNum)
+    finalBinaryValue=finalBinaryValue+binaryNum
+print('finalBinaryValue', finalBinaryValue)
+
+lengthOfFinalBinaryValue=len(finalBinaryValue)
+
+if lengthOfFinalBinaryValue<=7:
+    spacesRemaindingInByte=7-lengthOfFinalBinaryValue
+    zeroPad=''
+    while spacesRemaindingInByte>0:
+        zeroPad+='0'
+        spacesRemaindingInByte=spacesRemaindingInByte-1
+    print('UTF format ->','0'+zeroPad+finalBinaryValue)
