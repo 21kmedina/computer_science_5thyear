fb73c8c7f76366028c9dd7a0e5595571d000935c
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-11 20:43:51 +0000

    Add files via upload

---
+++ revision.assignment1/question2.py
@@ -0,0 +1,13 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 2,revision assignmenet1
+'''
+user_input=int(input('Enter a number :'))
+len_user_input=int(user_input+1)
+
+total=0
+for i in range(0,len_user_input):
+    total=total+i
+print(total)
+    
