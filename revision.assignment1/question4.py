ba941a5857759e867753d74dae9b919315dfd3b9
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-11 20:51:02 +0000

    Add files via upload

---
+++ revision.assignment1/question4.py
@@ -0,0 +1,13 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 4,revision assignmenet1
+'''
+
+product=1
+
+for i in range(1,51):
+    if i%2:
+        even_num=i%2
+    even_num = even_num * i
+print(even_num)
