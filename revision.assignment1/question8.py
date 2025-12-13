759d5ae24590397852b41cd403cfa26d1c8000b3
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-13 15:14:49 +0000

    Add files via upload

---
+++ revision.assignment1/question8.py
@@ -0,0 +1,10 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 8,revision assignmenet1
+'''
+multiple=1
+for i in range(1,51):
+    if (i%5==0):
+        multiple+=multiple*i
+print(multiple)
---
+++ revision.assignment1/question9.py
@@ -0,0 +1,10 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 9,revision assignmenet1
+'''
+double=0
+total=2
+for i in range(1,11):
+    double+=i*total
+print(double)
