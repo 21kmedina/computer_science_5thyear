d15e2a4ad66b14b4946eacd286da73a3d7e45828
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-13 14:39:44 +0000

    Fix calculation of sum of even numbers

--- revision.assignment1/question4.py
+++ revision.assignment1/question4.py
@@ -4,10 +4,9 @@ date:11th-dec-2025
 code:question 4,revision assignmenet1
 '''
 
-product=1
-
+total=0
 for i in range(1,51):
-    if i%2:
-        even_num=i%2
-    even_num = even_num * i
-print(even_num)
+    if i%2==0:
+        total=total+i
+print(total)
+ 
