e32e4cc54ebb765f02fcfb609e86009fee9e8523
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-13 14:42:53 +0000

    Update question5.py

--- revision.assignment1/question5.py
+++ revision.assignment1/question5.py
@@ -3,16 +3,11 @@ name:karla patino
 date:11th-dec-2025
 code:question 5,revision assignmenet1
 '''
-user_input=int(input('Enter a number :'))
-len_user_input=int(user_input+1)
-
-product=1
-for i in range(1,len_user_input):
-    if i%2:
-        even_num=i%2
-        even_num = even_num + 1
-        print(even_num)
-    else :
-        product=product+1
-    print(product)
+user_input=int(input('enter a number ;'))
+user_input=int(user_input+1)
+total=0
+for i in range(1,user_input):
+    if i%2==1:
+        total=total+i
+print(total)
         
