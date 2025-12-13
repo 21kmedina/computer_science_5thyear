618a058cbac2a1cce84725ddbcdcbaf0de481ebe
Karla <148641305+21kmedina@users.noreply.github.com>
2025-12-13 14:59:43 +0000

    Add files via upload

---
+++ revision.assignment1/question6.py
@@ -0,0 +1,13 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 6,revision assignmenet1
+'''
+multiple=0
+total=0
+for i in range(1,51):
+    multiple=total+i
+    if multiple%3==0:
+        final=multiple+multiple
+print(final)
+ 
---
+++ revision.assignment1/question7.py
@@ -0,0 +1,11 @@
+'''
+name:karla patino
+date:11th-dec-2025
+code:question 7,revision assignmenet1
+'''
+total=1
+for i in range(1,51):
+    if i%2==0:
+        product=total*i
+print(product)
+
