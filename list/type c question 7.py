accae9b6559b16c42a87bd8d9b84d61120bca04c
Karla <148641305+21kmedina@users.noreply.github.com>
2026-01-22 09:45:26 +0000

    Add files via upload

---
+++ list/type c question 7.py
@@ -0,0 +1,18 @@
+'''
+Author:Karla Patino Medina
+Date: 20/01/2026
+About: Programming practice TYPE C, question 7
+'''
+#Q7 a
+lst=[]
+for i in range(0,50,1):
+    lst.append(i)
+print(lst)
+
+#part 7 b
+lst2=[]
+for i in range(0,51):
+    x=i**2
+    lst2.append(x)
+print(lst2)
+    
