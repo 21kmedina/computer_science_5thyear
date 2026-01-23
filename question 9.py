17d92fb3429349a3083f1a77fdf09b360aaff72c
Karla <148641305+21kmedina@users.noreply.github.com>
2026-01-23 09:45:21 +0000

    Add files via upload

---
+++ question 9.py
@@ -0,0 +1,10 @@
+'''
+Author:Karla Patino Medina
+Date: 23/01/2026
+About: Programming practice TYPE C, question 9
+'''
+
+lst=eval(input('enter a list'))
+i=lst.pop(-1)
+lst.insert(0,i)
+print(lst)
