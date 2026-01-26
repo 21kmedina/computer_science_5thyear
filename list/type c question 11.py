30183767f04a83e3f00fc1d261888d370422efca
Karla <148641305+21kmedina@users.noreply.github.com>
2026-01-26 12:58:41 +0000

    Add files via upload

---
+++ list/type c question 11.py
@@ -0,0 +1,18 @@
+'''
+Author:Karla Patino Medina
+Date: 26/01/2026
+About: Programming practice TYPE C, question 11
+'''
+lst=eval(input('enter a list full of different variables:'))
+
+length2=''
+lenght_i=''
+lst=str(lst)
+
+for i in lst:
+    length_i=len(i)
+    len2=length_i
+    if length_i > len2:
+        print(length_i)
+    
+        
