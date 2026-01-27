27fa1809213b17e11cc903bd2ed386e898b98a0a
Karla <148641305+21kmedina@users.noreply.github.com>
2026-01-27 14:27:06 +0000

    Add files via upload

---
+++ list/type c question 12.py
@@ -0,0 +1,12 @@
+'''
+Author:Karla Patino Medina
+Date: 26/01/2026
+About: Programming practice TYPE C, question 12
+'''
+
+num=eval(input('Enter variables for the numerators :'))
+denum=eval(input('Enter variables for the denominators :'))
+
+min_num=min(num)
+max_denum=max(denum)
+print(min_num,'/',max_denum)
