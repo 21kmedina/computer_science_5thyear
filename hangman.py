9c3c559b5387d3bcbd850ae06994820567a7328d
Karla <148641305+21kmedina@users.noreply.github.com>
2026-01-30 09:41:13 +0000

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+'''
+author: Karla Patino
+date: 30th of January
+program:programm a hangman
+'''
+
+word= 'Concatenate'
+
+print('Welcome to Hangman.Please save a life guessing the correct word (:')
+length=len(word)
+print('Guess the word:','_ '*length)
+
+turns_left=14
+
+while turns!=0:
+    User_guess=input('Enter a character:')
+    if User_guess in word:
+        
+
+
