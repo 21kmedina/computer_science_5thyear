3bfac85eacaef5993f6ccdfa34c9ddadb76ff2de
Karla <148641305+21kmedina@users.noreply.github.com>
2026-02-12 09:46:42 +0000

    Add files via upload

---
+++ random exercises/exra.py
@@ -0,0 +1,22 @@
+'''
+author:karla patino
+date:12/02/2026
+importing random exercises, Coding assignment
+program 1- two digit numbers
+'''
+
+user_num1=input('Enter a first number with two digits:')
+user_num1_reversed=str(user_num1)
+user_num1_reversed=user_num1_reversed[::-1]
+print(user_num1_reversed)
+user_num1=int(user_num1)
+
+user_num2=input('Enter a second number with two digits:')
+user_num2_reversed=str(user_num2)
+user_num2_reversed=user_num2_reversed[::-1]
+print(user_num2_reversed)
+user_num2=int(user_num2)
+
+
+
+
