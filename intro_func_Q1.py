3a07348b13d5caf93840e38e8a340b001a4eba4d
Karla <148641305+21kmedina@users.noreply.github.com>
2026-04-30 09:40:38 +0100

    Add files via upload

---
+++ intro_func_Q1.py
@@ -0,0 +1,20 @@
+# Karla Patino medina
+#30-04-2026
+#Intro to functions exercises
+
+#Q1
+
+def even_or_odd(a,b):
+    if (a%2==0) and (b%2==0):
+        if a<b:
+            result=a
+        elif b<a:
+            result=b
+    else:
+        if a>b:
+            result=a
+        elif b>a:
+            result=b
+    return (result)
+x= even_or_odd(3,7)
+print(x)
