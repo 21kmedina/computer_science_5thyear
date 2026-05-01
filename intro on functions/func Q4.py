049371cea3db305db58788ef107bb59e9852bcc6
Karla <148641305+21kmedina@users.noreply.github.com>
2026-05-01 09:44:20 +0100

    Add files via upload

---
+++ intro on functions/func Q4.py
@@ -0,0 +1,12 @@
+#karla patino
+#1-5-2026,  Functions Q4
+
+#old macdonald
+def Name(a):
+    if len(a)>4:
+        pos_1=a[0]
+        pos_2=a[3]
+        pos_1=pos_1.upper
+        pos_2=pos_2.upper
+        
+a=input('Enter a name:')
