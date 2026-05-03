2f1c25e04ca0a29d7f8072d6ed184fa3a39b7600
Karla <148641305+21kmedina@users.noreply.github.com>
2026-05-03 15:06:02 +0100

    Add files via upload

---
+++ intro on functions/functions Q5.py
@@ -0,0 +1,12 @@
+# Karla Patino Medina
+# 02-05-26
+
+# Q5- Master Yoda
+def reverse_order(sentence):
+    space=sentence.find(' ')
+    for i in sentence:
+        if i != space:
+            word1=sentence
+        print(word1)
+
+sentence=input('Enter a sentence :')
