2091cb7fbfe7fe8b46cd311688c3857a15e61337
Karla <148641305+21kmedina@users.noreply.github.com>
2026-05-11 12:30:13 +0100

    Refactor functions for statistical calculations

--- func ex2.py
+++ func ex2.py
@@ -29,6 +29,30 @@ def median(b):
         med_lst=len(lst)/2
         indx=int(round(med_lst,1))
         result=lst[indx]
-        
-    
-    
+
+    elif len(lst)%2==0:
+        lst=sorted(lst)
+        med_lst=len(lst)/2
+        indx=int(round(med_lst,1))
+        med_lst=(lst[indx]+lst[indx-1])
+        med_lst=med_lst/2
+        result=med_lst
+    return result
+
+b = median(lst)
+print('Median value :',b)
+
+
+#Part 4
+def mode_val(lst):
+    for i in lst:
+        count=lst.count(i)
+        if count > 1 :
+            mode=i
+            break
+        else:
+            mode='Nothing'           
+    return mode
+
+c=mode_val(lst)
+print('The mode value is :',c)
