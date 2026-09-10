ba2db9471fccd38f01da4db8b1ce066aeb8db26c
Karla <148641305+21kmedina@users.noreply.github.com>
2026-09-10 10:19:18 +0100

    Update simple sort.py

--- simple sort.py
+++ simple sort.py
@@ -1,19 +1,27 @@
-#simple sort
+#simple sort,karla patino 10th of september#
 lst=[2,5,4]
 
 new_lst=[]
 
-len_lst=len(lst)
-val=0
+len_lst=len(lst) +1
+val=len_lst
 
-while len_lst > val:
-    p1=lst[0]
-    p2=lst[1]
-    if p1 < p2:
-        new_lst.append(p1)
-        lst.remove(p1)
-        val+=1
-        
-        print(new_lst,lst)
+while len_lst >= val:
+    len_lst-=1
+    if len_lst>1:
+        p1=lst[0]
+        p2=lst[1]
+        if p1 < p2:
+            new_lst.append(p1)
+            lst.remove(p1)
+            val-=1
+        elif p2 < p1:
+            new_lst.append(p2)
+            lst.remove(p2)
+            val-=1
+    else:
+          new_lst.append(p1)
+          lst.remove(p1)
+print(new_lst,lst)
     
     
